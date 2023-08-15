76cf7b422d3e63fb8dfb96b75bf6e66b136b7244
Felipe <felipegraca.1999@gmail.com>
2023-08-15 09:09:38 -0300

    feature: create user dto and user antity

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,47 @@
-export class CreateUserDto {}
+import {
+  IsBoolean,
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends User {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  cpf: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
+  password: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password too weak',
+  })
+  password_confirmation: string;
+
+  @IsString()
+  @MinLength(10)
+  @MaxLength(11)
+  phone: string;
+
+  @IsBoolean()
+  isNewUser: boolean;
+
+  @IsBoolean()
+  isSocialMedia: boolean;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+export class User {
+  id?: string;
+  name: string;
+  email: string;
+  cpf: string;
+  password: string;
+  password_confirmation: string;
+  phone: string;
+  isNewUser: boolean;
+  isSocialMedia: boolean;
+  lastSession: Date;
+  createdAt: Date;
+}
