5c1ef76b5c6f536cea7517cf740d6a0a71c39f5f
Felipe <felipegraca.1999@gmail.com>
2023-08-15 08:53:56 -0300

    chore: install dependences auth

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM node:19-slim
-
-WORKDIR /home/node/app
-
-CMD ["tails", "-f", "/dev/null"]
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AppService } from './service/app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { AppController } from './controller/app.controller';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
